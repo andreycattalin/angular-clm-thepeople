7c7a59d5442effe628ecfe344fea7940b0f0637b
Andrey <cattalin.htc@gmail.com>
2021-03-25 09:46:36 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { DetailComponent } from './detail/detail.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [					
+    AppComponent,
+      MenuComponent,
+      FooterComponent,
+      HomeComponent,
+      SearchComponent,
+      DetailComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
